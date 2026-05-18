9263a8051f686b35a2ef8cd1b26737d6e7b735f6
TanvirHassan-official <tanvirhassan0006@gmail.com>
2026-05-18 12:46:31 +0600

    not found updated

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,24 +1,22 @@
-/* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
+import Image from "next/image";
 import 'animate.css';
-
-
 
 const NotFound = () => {
     return (
         <div className="grid justify-items-center gap-10 m-10">
-
-            <img
-              className="max-h-96 w-auto drop-shadow-2xl rounded-2xl animate__animated animate__tada"
-              src={"/assets/error-404.png"}
-              alt="Clicking for the best tools"
+            <Image
+                className="max-h-96 w-auto drop-shadow-2xl rounded-2xl animate__animated animate__tada"
+                src="/assets/error-404.png"
+                alt="404 not found"
+                width={500}
+                height={400}
             />
             <Link href="/">
-                <button className={'btn bg-[#386FA4] text-white hover:bg-white hover:text-black px-10 '}>
-                Back to Home
+                <button className="btn bg-[#386FA4] text-white hover:bg-white hover:text-black px-10">
+                    Back to Home
                 </button>
             </Link>
-
         </div>
     );
 };
